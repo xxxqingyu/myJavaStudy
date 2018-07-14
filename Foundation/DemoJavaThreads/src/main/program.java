1c842d5dd3e3e229d679527043bef1eac4281c2a
xxxqingyu <xuxuan819@163.com>
2018-07-14 17:57:25 +0800

    修改foundation

---
+++ Foundation/DemoJavaThreads/src/main/program.java
@@ -0,0 +1,17 @@
+package main;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		program.Test();
+		program ins=new program();
+		ins.Test();
+
+	}
+	
+	public static void Test(){
+		System.out.println("test");
+	}
+	
+}
