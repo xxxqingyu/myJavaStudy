885f8f5b33b626e662e2c6c5fedfe87853eed0a6
xuxuan <xuxuan819@163.com>
2018-07-23 16:43:36 +0800

    增加crud方法

--- Others/JavaWebFramework/chapter1/src/main/java/org/smart4j/chapter2/helper/DatabaseHelper.java
+++ Others/JavaWebFramework/chapter1/src/main/java/org/smart4j/chapter2/helper/DatabaseHelper.java
@@ -3,14 +3,22 @@ package org.smart4j.chapter2.helper;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.utils.CollectionUtil;
 import org.smart4j.chapter2.utils.PropsUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DatabaseHelper {
 	private static final Logger LOGGER=LoggerFactory.getLogger(DatabaseHelper.class);
@@ -50,6 +58,100 @@ public class DatabaseHelper {
 			closeConnection();
 		}
 		return entityList;
+	}
+	
+	public static <T> T queryEntity(Class<T> entityClass,String sql,Object...params){
+		T entity;
+		try{
+			Connection connection=getConnection();
+			entity= QUERY_RUNNER.query(connection, sql, new BeanHandler<T>(entityClass),params);
+		}catch (SQLException e) {
+			LOGGER.error("query entity list failure",e);
+			throw new RuntimeException(e);
+		}finally {
+			closeConnection();
+		}
+		return entity;
+	}
+	
+	public static List<Map<String, Object>> executeQuery(String sql,Object... params) {
+		List<Map<String, Object>> result;
+		try {
+			Connection connection=getConnection();
+			result = QUERY_RUNNER.query(connection, sql, new MapListHandler(), params);
+		} catch (Exception e) {
+			LOGGER.error("execute entity  failure",e);
+			throw new RuntimeException(e);
+		}finally {
+			closeConnection();
+		}
+		return result;
+	}
+	
+	public static int executeUpdate(String sql,Object... params) {
+		int rows;
+		try {
+			Connection connection=getConnection();
+			rows = QUERY_RUNNER.update(connection, sql, params);
+		} catch (Exception e) {
+			LOGGER.error("execute entity  failure",e);
+			throw new RuntimeException(e);
+		}finally {
+			closeConnection();
+		}
+		return rows;
+	}
+	
+	public static <T> boolean insertEntity(Class<T> entotyClass,Map<String, Object> fieldMap) {
+		if (CollectionUtil.isEmpty(fieldMap)) {
+			LOGGER.error("can not insert entity:field is empty");
+			return false;
+		}
+		String sql ="INSERT INTO "+ getTableName(entotyClass);
+		StringBuilder columns=new StringBuilder("(");
+		StringBuilder values=new StringBuilder("(");
+		for (String fieldName : fieldMap.keySet()) {
+			columns.append(fieldName).append(", ");
+			values.append("?, ");
+		}
+		columns.replace(columns.lastIndexOf(", "), columns.length(), ")");
+		values.replace(values.lastIndexOf(", "), values.length(), ")");
+		
+		sql+= columns+ " Values " + values;
+		
+		Object[] params=fieldMap.values().toArray();
+		
+		return executeUpdate(sql, params)==1;
+	}
+	
+	public static String getTableName(Class<?> entityClass) {
+		return entityClass.getSimpleName();
+	}
+	
+	public static <T> boolean  updateEntity(Class<T> entityClass,long id,Map<String, Object> fieldMap)  {
+		if (CollectionUtil.isEmpty(fieldMap)) {
+			LOGGER.error("can not update entoty: fieldMap is empty");
+			return false;
+		}
+		
+		String sql="UPDATE "+getTableName(entityClass) + " SET ";
+		StringBuilder columns = new StringBuilder();
+		for (String fieldName : fieldMap.keySet()) {
+			columns.append(fieldName).append("=?, ");
+		}
+		sql += columns.substring(0, columns.lastIndexOf(", ")) + " WHERE id=?";
+		
+		List<Object> paramList=new ArrayList<>();
+		paramList.addAll(fieldMap.values());
+		paramList.add(id);
+		Object[] params = paramList.toArray();
+		
+		return executeUpdate(sql, params) == 1;
+	}
+	
+	public static <T> boolean deleteEntity(Class<T> entityClass,long id) {
+		String sql="DELETE FROM "+getTableName(entityClass) + " WHERE id=? ";
+		return executeUpdate(sql, id) == 1;
 	}
 	
 	/**
--- Others/JavaWebFramework/chapter1/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ Others/JavaWebFramework/chapter1/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,5 +1,9 @@
 package org.smart4j.chapter2.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,11 +20,53 @@ public class CustomerService {
 		
 		String sql="SELECT * FROM customer";
 		
-		return DatabaseHelper.queryEntityList(Customer.class, sql);
-		
+		return DatabaseHelper.queryEntityList(Customer.class, sql);	
+	}
+	
+	public List<Customer> getCustomerList2(){
+		Connection connection=null;
+		try {
+			List<Customer> customerList=new ArrayList<>();
+			String sql="SELECT * FROM customer where id=1;SELECT * FROM customer where id=2;SELECT * FROM customer2 where id=3";
+			
+			connection= DatabaseHelper.getConnection();
+			PreparedStatement statement=connection.prepareStatement(sql);
+			ResultSet resultSet=statement.executeQuery();
+			
+			while (resultSet.next()) {
+				Customer customer=new Customer();
+				customer.setId(resultSet.getLong("id"));
+				customer.setName(resultSet.getString("name"));
+				customer.setContact(resultSet.getString("contact"));
+				customer.setTelephone(resultSet.getString("telephone"));
+				
+				customerList.add(customer);
+			}
+			
+			while (statement.getMoreResults()) {
+				resultSet =	statement.getResultSet();
+				while (resultSet.next()) {
+					Customer customer=new Customer();
+					customer.setId(resultSet.getLong("id"));
+					customer.setName(resultSet.getString("name"));
+					customer.setContact(resultSet.getString("contact"));
+					customer.setTelephone(resultSet.getString("telephone"));
+					customerList.add(customer);
+				}
+				
+			}
+			
+			return customerList;
+		} catch (Exception e) {
+			LOGGER.error("execute sql failure",e);
+		}finally {
+			DatabaseHelper.closeConnection();
+		}
+		return null;
 	}
 	
 	public Customer getCustomer(long id){
+		
 		return null;
 	}
 	
