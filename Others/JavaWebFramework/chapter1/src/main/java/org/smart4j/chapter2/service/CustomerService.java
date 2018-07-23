3eadd92f5345d3924dcc164436851dce2bd221ff
xuxuan <xuxuan819@163.com>
2018-07-23 11:45:26 +0800

    移除多余import

--- Others/JavaWebFramework/chapter1/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ Others/JavaWebFramework/chapter1/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,21 +1,12 @@
 package org.smart4j.chapter2.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
-import org.smart4j.chapter2.utils.PropsUtil;
 
 public class CustomerService {
 	private static final Logger LOGGER=LoggerFactory.getLogger(CustomerService.class);
