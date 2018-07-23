f2d06606697629a0cbff6b66ea38c871f6b0a7d1
xuxuan <xuxuan819@163.com>
2018-07-23 11:10:31 +0800

    增加databaseHelper

--- Others/JavaWebFramework/chapter1/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ Others/JavaWebFramework/chapter1/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -4,55 +4,46 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
 import org.smart4j.chapter2.utils.PropsUtil;
 
 public class CustomerService {
 	private static final Logger LOGGER=LoggerFactory.getLogger(CustomerService.class);
 	
-	private static final String DRIVER;
-	private static final String	URL;
-	private static final String USERNAME;
-	private static final String PASSWORD;
-	
-	static{
-		Properties conf=PropsUtil.loadProps("config.properties");
-		DRIVER=conf.getProperty("jdbc.driver");
-		URL=conf.getProperty("jdbc.url");
-		USERNAME=conf.getProperty("jdbc.username");
-		PASSWORD=conf.getProperty("jdbc.password");
-		
-		try {
-			Class.forName(DRIVER);
-		} catch (ClassNotFoundException e) {
-			LOGGER.error("can not load jdbc driver",e);
-		}
-	}
 	
 	public List<Customer> getCustomerList(){
 		Connection connection=null;
 		try {
 			List<Customer> customerList=new ArrayList<>();
 			String sql="SELECT * FROM customer";
-			connection= DriverManager.getConnection(URL);
+			connection= DatabaseHelper.getConnection();
 			PreparedStatement statement=connection.prepareStatement(sql);
 			ResultSet resultSet=statement.executeQuery();
 			while (resultSet.next()) {
 				Customer customer=new Customer();
 				customer.setId(resultSet.getLong("id"));
+				customer.setName(resultSet.getString("name"));
+				customer.setName(resultSet.getString("contact"));
+				customer.setName(resultSet.getString("telephone"));
+				customer.setName(resultSet.getString("email"));
+				customer.setName(resultSet.getString("remark"));
 				customerList.add(customer);
 			}
+			return customerList;
 		} catch (Exception e) {
-			// TODO: handle exception
+			LOGGER.error("execute sql failure",e);
 		}finally {
-			
+			DatabaseHelper.closeConnection(connection);
 		}
 		return null;
 	}
