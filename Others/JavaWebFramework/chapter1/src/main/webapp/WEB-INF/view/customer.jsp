3d4800e020310a72e761711c619c9198f24b9fd6
xxxqingyu <xuxuan819@163.com>
2018-07-24 22:54:04 +0800

    增加ClassHelper

--- Others/JavaWebFramework/chapter1/src/main/webapp/WEB-INF/view/customer.jsp
+++ Others/JavaWebFramework/chapter1/src/main/webapp/WEB-INF/view/customer.jsp
@@ -2,8 +2,8 @@
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <c:set var="BASE" value="${pageContext.request.contextPath}"></c:set>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>客户管理</title>
--- Others/JavaWebFramework/smart-framework/src/main/java/org/smart4j/smart_framework/helper/ClassHelper.java
+++ Others/JavaWebFramework/smart-framework/src/main/java/org/smart4j/smart_framework/helper/ClassHelper.java
@@ -1,5 +1,52 @@
 package org.smart4j.smart_framework.helper;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.smart4j.smart_framework.annotation.Controller;
+import org.smart4j.smart_framework.annotation.Service;
+import org.smart4j.smart_framework.util.ClassUtil;
+
+
 public final class ClassHelper {
+	private static final Set<Class<?>> CLASS_SET;
 	
+	static{
+		String basePackage=ConfigHelper.getAppBasePackage();
+		CLASS_SET=ClassUtil.getClasses(basePackage, true);
+	}
+	
+	/**
+	 * @return
+	 */
+	public static Set<Class<?>> getClassSet() {
+		return CLASS_SET;
+	}
+	
+	public static Set<Class<?>> getServiceClassSet() {
+		Set<Class<?>>  classSet=new HashSet<>();
+		for (Class<?> cls : CLASS_SET) {
+			if (cls.isAnnotationPresent(Service.class)) {
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}
+	
+	public static Set<Class<?>> getControllerClassSet() {
+		Set<Class<?>>  classSet=new HashSet<>();
+		for (Class<?> cls : CLASS_SET) {
+			if (cls.isAnnotationPresent(Controller.class)) {
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}
+	
+	public static Set<Class<?>> getBeanClassSet() {
+		Set<Class<?>>  classSet=new HashSet<>();
+		classSet.addAll(getServiceClassSet());
+		classSet.addAll(getControllerClassSet());
+		return classSet;
+	}
 }
